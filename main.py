0b4268375eb52f93f366fbd3420ec2ea2b105a5b
Pedro Odake <pedroodake@gmail.com>
2025-04-09 20:35:00 -0300

    NPCs do mapa 1 adicionados

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import player
 import inimigo
 # Ajuste os imports de inimigo conforme necessário
 from inimigo import Inimigo1mp1, Inimigo1mp2 # Exemplo para mapa 1
+from npc import NPC_Andy, NPC_Rafa
 # from inimigo import InimigoTipoA, InimigoTipoB # Exemplo para mapa 2 (precisaria existir em inimigo.py)
 from map_loader import carregar_mapa, desenhar_mapa, criar_mapa_rects, criar_objetos_retangulos
 
@@ -125,6 +126,14 @@ class Game:
         self.inimigos = pygame.sprite.Group()
         self.todos_sprites = pygame.sprite.Group()
 
+        # VARIAVEIS DA CONVERSA
+        self.em_conversa = False
+        self.tempo_conversa = 0
+        self.duracao_conversa = 7000  # milissegundos (7 segundos)
+        self.npc_A = None
+        self.npc_F = None
+        self.npc = None
+
         self.mapa_atual_path = "Mapa.tmx"
         self.proximo_mapa_path = "Mapa(2).tmx"
 
@@ -216,6 +225,14 @@ class Game:
         pygame.display.flip()
         pygame.time.wait(3000)
         self.game_state = "MENU"
+        
+    # MENSAGEM NPC
+    def exibir_mensagem_npc(self, mensagem, cor):
+        overlay = pygame.Surface((self.LARGURA, self.ALTURA), pygame.SRCALPHA)
+        overlay.fill((0, 0, 0, 180)); self.tela.blit(overlay, (0, 0))
+        self.draw_text(mensagem, 74, self.LARGURA // 2, self.ALTURA // 2, color=cor) # Usa draw_text da classe
+        pygame.display.flip(); pygame.time.wait(3000)
+        # self.game_state = "PLAYING"
 
     def exibir_popup(self, mensagem):
         self.popup_mensagem = mensagem
@@ -352,6 +369,8 @@ class Game:
         self.inimigos.empty()
         self.todos_sprites.empty()
         self.jogador = None
+        self.npc_A = None
+        self.npc_F = None
         self.colisao_rects = []
         self.rampas_esquerda_rects = []
         self.rampas_direita_rects = []
@@ -486,11 +505,23 @@ class Game:
             import traceback; traceback.print_exc()
             self.game_state = "MENU"
             return
+        
 
         # --- 5. Criar Inimigos (CONDICIONAL POR MAPA) ---
         print(f"DEBUG: Criando inimigos para {map_path}...")
         lista_inimigos_mapa = []
         if map_path == "Mapa.tmx":
+            # Fazer NPC
+            try:
+                self.npc_A = NPC_Andy(x=208, y=398, zoom_level=self.zoom_level)
+                self.npc_F = NPC_Rafa(x=5667, y=398, zoom_level=self.zoom_level)
+                self.todos_sprites.add(self.npc_A, self.npc_F)
+                print("DEBUG: NPCs criados no Mapa 1.")
+            except Exception as e:
+                print(f"ERRO CRÍTICO ao criar NPCs no Mapa 1: {e}")
+                self.game_state = "MENU"
+                return
+            
             try:
                 inimigo1 = inimigo.Inimigo1mp1(x=2950, y=0, jogador=self.jogador, colisao_rects=self.colisao_rects, tmx_data=self.tmx_data, largura_mapa=self.largura_mapa, altura_mapa=self.altura_mapa_real)
                 inimigo2 = inimigo.Inimigo1mp2(x=4150, y=214, jogador=self.jogador, colisao_rects=self.colisao_rects, tmx_data=self.tmx_data, largura_mapa=self.largura_mapa, altura_mapa=self.altura_mapa_real)
@@ -515,16 +546,29 @@ class Game:
              print(f"DEBUG: {len(lista_inimigos_mapa)} inimigos adicionados ao mapa.")
         else:
             print("DEBUG: Nenhum inimigo definido ou criado para este mapa.")
+        
 
         # --- 6. Resetar Câmera e Estado Final ---
         self.deslocamento_camera_x = 0
         self.deslocamento_camera_y = 0
         self.mostrar_popup = False
         self.mostrar_prompt_porta = False
+        self.mostrar_msg_npc = False
         self.game_state = "PLAYING"
         print(f"DEBUG: start_game concluído. Estado definido para PLAYING.")
 
 
+    def exibir_conversa(self):
+    # Escurece o fundo
+        overlay = pygame.Surface((self.LARGURA, self.ALTURA), pygame.SRCALPHA)
+        overlay.fill((60, 60, 60, 12))
+        self.tela.blit(overlay, (0, 0))
+
+        if self.npc:  # Verifica se há um NPC com quem conversar
+            self.npc.conversar(self.tela, self.LARGURA, self.ALTURA)
+
+        pygame.display.flip()
+    
     def run(self):
         """Loop principal do jogo que gerencia estados, eventos, atualizações e desenho."""
         print("DEBUG: Iniciando loop principal (run)")
@@ -553,17 +597,49 @@ class Game:
                      self.game_state = "MENU"; continue
 
                 # --- Processar Eventos do Jogo ---
-                self.mostrar_prompt_porta = False
                 colidindo_com_porta = False
                 if self.porta_rects:
                      for porta in self.porta_rects:
                          if self.jogador.collision_rect.colliderect(porta):
                              self.mostrar_prompt_porta = True
                              colidindo_com_porta = True; break
+                             
+                # CODIGO PARA APARECER O TEXTO DE CONVERSA COM NPC
+                self.mostrar_msg_npc = False
+                npc_proximo = None
+                distancia_limite = 100  # Distância máxima para interação
+
+                jogador_x, jogador_y = self.jogador.rect.center
+
+                for npc in [self.npc_A, self.npc_F]:
+                    npc_x, npc_y = npc.rect.center
+                    distancia = ((jogador_x - npc_x) ** 2 + (jogador_y - npc_y) ** 2) ** 0.5
+
+                    if distancia < distancia_limite:
+                        self.mostrar_msg_npc = True
+                        npc_proximo = npc
+                        break
+
+                if npc_proximo:
+                    self.npc = npc_proximo  
+                else:
+                    self.npc = None  
 
                 for evento in eventos_frame:
                     if evento.type == pygame.QUIT: executando = False
                     if evento.type == pygame.KEYDOWN:
+                        if self.em_conversa and self.npc:
+                            if evento.key == pygame.K_RIGHT:
+                                self.npc.contador += 1
+                                if self.npc.contador > self.npc.total_falas:
+                                    self.em_conversa = False
+                                    self.npc.contador = 1  
+
+                            elif evento.key == pygame.K_LEFT:
+                                if self.npc.contador > 1:
+                                    self.npc.contador -= 1
+                                else:
+                                    self.npc.contador = 1  # Garante que não vá abaixo de 1
                         if evento.key == pygame.K_ESCAPE: self.game_state = "MENU"
                         elif evento.key == pygame.K_e: # Tecla 'E'
                              if colidindo_com_porta: # Se está na porta E pressionou E
@@ -589,6 +665,21 @@ class Game:
                                 self.exibir_popup(msg)
                         elif evento.key == pygame.K_LSHIFT or evento.key == pygame.K_RSHIFT: self.jogador.iniciar_dash()
                         elif evento.key == pygame.K_z: self.jogador.atacar(list(self.inimigos))
+                        elif evento.key == pygame.K_f:
+                                jogador_x, jogador_y = self.jogador.rect.center
+                                npc_proximo = None
+                                menor_distancia = float("inf")
+                                for npc in [self.npc_A, self.npc_F]:
+                                    npc_x, npc_y = npc.rect.center
+                                    distancia = ((jogador_x - npc_x) ** 2 + (jogador_y - npc_y) ** 2) ** 0.5
+                                    if distancia < 100 and distancia < menor_distancia:
+                                        menor_distancia = distancia
+                                        npc_proximo = npc
+
+                                if npc_proximo:
+                                    self.npc = npc_proximo
+                                    self.em_conversa = True
+                                                                       
                         # Debug keys...
 
                 # --- Atualizações do Jogo ---
@@ -596,7 +687,12 @@ class Game:
                     # Atualizar Jogador (inclui checagem de buraco/lava agora)
                     self.jogador.atualizar(self.inimigos)
                     self.jogador.update_animation()
-
+                    if self.em_conversa:
+                        self.exibir_conversa()
+                        continue 
+                    self.npc_A.atualizar()
+                    self.npc_F.atualizar()
+                    
                     # Atualizar Inimigos
                     for inimigo_atual in self.inimigos:
                         if hasattr(inimigo_atual, 'update') and callable(inimigo_atual.update):
@@ -647,12 +743,17 @@ class Game:
                     if altura_mapa_escalada > self.ALTURA: self.deslocamento_camera_y = max(self.ALTURA - altura_mapa_escalada, self.deslocamento_camera_y)
                     else: self.deslocamento_camera_y = (self.ALTURA - altura_mapa_escalada) / 2
 
+                
                     # --- Desenhar Jogo ---
                     # 1. Mapa com Zoom
                     self.desenhar_mapa_com_zoom()
+                    
 
                     # 2. Todos os Sprites (Inimigos e Jogador)
                     sprites_para_desenhar = list(self.inimigos) + [self.jogador]
+                    if self.npc_A: sprites_para_desenhar.append(self.npc_A)
+                    if self.npc_F: sprites_para_desenhar.append(self.npc_F)
+                    
                     for sprite in sprites_para_desenhar:
                          if sprite:
                              try:
@@ -675,6 +776,8 @@ class Game:
                     self.desenhar_coracoes()
                     self.desenhar_pocoes()
                     self.desenhar_popup()
+                    if self.mostrar_msg_npc:
+                        self.draw_text("Pressione F para conversar", 30, self.LARGURA // 2, self.ALTURA - 30, color=self.AMARELO, font_name=self.default_font_name, center=True)
                     if self.mostrar_prompt_porta:
                         self.draw_text("Pressione E para próxima fase", 30, self.LARGURA // 2, self.ALTURA - 30, color=self.AMARELO, font_name=self.default_font_name, center=True)
                     # Debug info...
---
+++ npc.py
@@ -0,0 +1,133 @@
+import pygame
+
+PARADO_ANDY = 'parado A'
+PARADO_RAFA = 'parado F'
+
+SPRITES = {
+    PARADO_ANDY: {"file": "img/npc/andy.png", "frames": 2, "width": 635, "height": 941},
+    PARADO_RAFA: {"file": "img/npc/rafa.png", "frames": 3, "width": 261, "height": 394}
+}
+
+class NPC_Andy(pygame.sprite.Sprite):
+    def __init__(self, x, y, zoom_level=2.0):
+        super().__init__()
+        self.state = PARADO_ANDY
+        self._sprites_cache = {}
+        self.facing_right = True
+        self.animation_timer = 0
+        self.animation_speed = 8
+        self.frame_index = 0
+        self.zoom_level = zoom_level
+
+        self.load_sprites()
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_rect(topleft=(x, y))
+        
+        # DEF DE CONVERSAR COM O ANDY
+        self.contador = 1  # Começa na fala 1
+        self.total_falas = 4  # Total de imagens de fala
+        
+
+
+    def load_sprites(self):
+        sprite_info = SPRITES[self.state]
+        self.frames = self.load_frames(sprite_info["file"], sprite_info["frames"], sprite_info["width"], sprite_info["height"], (33, 48))
+
+
+    def load_frames(self, file, frame_count, width, height, resize):
+        """Separa os frames da sprite sheet e os redimensiona se necessário."""
+        sheet = pygame.image.load(file).convert_alpha()
+        frames = []
+        
+        for i in range(frame_count):
+            x = i * width
+            frame = sheet.subsurface(pygame.Rect(x, 0, width, height))
+            frame = pygame.transform.scale(frame, resize)
+            frames.append(frame)
+        
+        return frames
+
+    def atualizar(self):
+        self.animation_timer += 1
+        if self.animation_timer >= self.animation_speed:
+            self.animation_timer = 0
+            self.frame_index = (self.frame_index + 1) % len(self.frames)
+            self.image = self.frames[self.frame_index]
+            
+    def conversar(self, tela, largura_tela, altura_tela):
+        caminho = f"img/npc/fala_andy{self.contador}.png"
+
+        imagem = pygame.image.load(caminho).convert_alpha()
+        largura_fala = int(largura_tela * 0.7)
+        altura_fala = int(altura_tela * 0.25)
+        imagem = pygame.transform.scale(imagem, (largura_fala, altura_fala))
+
+        pos_x = (largura_tela - largura_fala) // 2
+        pos_y = altura_tela - altura_fala - 30
+
+        tela.blit(imagem, (pos_x, pos_y))
+        
+        
+class NPC_Rafa(pygame.sprite.Sprite):
+    def __init__(self, x, y, zoom_level=2.0):
+        super().__init__()
+        self.state = PARADO_RAFA
+        self._sprites_cache = {}
+        self.facing_right = True
+        self.animation_timer = 0
+        self.animation_speed = 8
+        self.frame_index = 0
+        self.zoom_level = zoom_level
+
+        self.load_sprites()
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_rect(topleft=(x, y))
+        
+        # DEF DE CONVERSAR COM O ANDY
+        self.contador = 1  # Começa na fala 1
+        self.total_falas = 3  # Total de imagens de fala
+
+
+    # def load_sprites(self):
+    #     sprite_info = SPRITES[self.state]
+    #     self.frames = self.load_frames(sprite_info["file"], sprite_info["frames"], sprite_info["width"], sprite_info["height"], (33, 48))
+        
+    def load_sprites(self):
+        sprite_info = SPRITES[self.state]
+        self.frames = self.load_frames(sprite_info["file"], sprite_info["frames"], sprite_info["width"], sprite_info["height"], (33, 48))
+        print(f"{self.__class__.__name__} - Frames carregados: {len(self.frames)}")
+
+
+    def load_frames(self, file, frame_count, width, height, resize):
+        """Separa os frames da sprite sheet e os redimensiona se necessário."""
+        sheet = pygame.image.load(file).convert_alpha()
+        frames = []
+        
+        for i in range(frame_count):
+            x = i * width
+            frame = sheet.subsurface(pygame.Rect(x, 0, width, height))
+            frame = pygame.transform.scale(frame, resize)
+            frames.append(frame)
+        return frames
+        
+
+
+    def atualizar(self):
+        self.animation_timer += 1
+        if self.animation_timer >= self.animation_speed:
+            self.animation_timer = 0
+            self.frame_index = (self.frame_index + 1) % len(self.frames)
+            self.image = self.frames[self.frame_index]
+            
+    def conversar(self, tela, largura_tela, altura_tela):
+        caminho = f"img/npc/fala_fantasma{self.contador}.png"
+
+        imagem = pygame.image.load(caminho).convert_alpha()
+        largura_fala = int(largura_tela * 0.7)
+        altura_fala = int(altura_tela * 0.25)
+        imagem = pygame.transform.scale(imagem, (largura_fala, altura_fala))
+
+        pos_x = (largura_tela - largura_fala) // 2
+        pos_y = altura_tela - altura_fala - 30
+
+        tela.blit(imagem, (pos_x, pos_y))
